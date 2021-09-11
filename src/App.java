7c979337197d8b0bef9dab4af634996b5699e6d8
daltonfranco <daltonpacolafranco.git@hotmail.com>
2021-09-10 22:57:06 -0300

    alteracoes

--- src/App.java
+++ src/App.java
@@ -9,8 +9,5 @@ public class App {
         //fat.fat(numero parar efetuar o fatorial);
         //cond.condicao();
         //od.mult();
-        Encadeado encad = new Encadeado();
-
-        encad.encad();
     }
 }
--- src/Encadeado.java
+++
@@ -1,6 +0,0 @@
-public class Encadeado {
-    public void encad(){
-
-        
-    }
-}
